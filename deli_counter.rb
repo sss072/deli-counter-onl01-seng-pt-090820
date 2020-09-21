4a97966dfe119e4e1ec6f258cd44c58efc3e3b16
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-21 00:44:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(katz_deli)
   if katz_deli == [] 
     puts "The line is currently empty."
   else 
-    current_line = "The line is currently:"
-    katz_deli.each_with_index do |element, index
+    new_line = "The line is currently:"
+    katz_deli.each_with_index do |element, index| 
+      new_line << "#{index + 1}. #{element}"
     
   end
 end
